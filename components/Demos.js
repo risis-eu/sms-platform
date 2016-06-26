d74799458357e837f0f56f0a469549afd5797f23
ali1k <hyperir@gmail.com>
2016-06-26 19:54:06 +0200

    added add-ons demo

--- components/Demos.js
+++ components/Demos.js
@@ -11,14 +11,15 @@ class Demos extends React.Component {
         return (
             <div className="ui page grid" ref="demos">
                 <div className="ui column segment">
-                    List of Demo Applications that use SMS Linked Data API:
-                    <div className="ui divided list">
-                        <div className="item">- <a href="/demos/geo/addressToAdmin">Address to Administrative Boundaries (uses all available shape files)</a></div>
-                        <div className="item">- <a href="/demos/geo/addressToGADM28Admin">Address to GADM Administrative Boundaries</a></div>
-                        <div className="item">- <a href="/demos/geo/addressToOSMAdmin">Address to OSM Administrative Boundaries</a></div>
-                        <div className="item">- <a href="/demos/geo/addressToFlickrAdmin">Address to Flickr Administrative Boundaries</a></div>
-                        <div className="item">- <a href="/boundariesMap">Example of Administrative Boundaries extracted for a specific country</a></div>
-                        <div className="item">- <a href="/demos/geo/NL_Universities/1000/1000">Educational Institutes in the Netherlands</a></div>
+                    List of Demo Applications that use SMS Linked Data API:<br/>
+                <div className="ui divided animated list">
+                        <div className="item">-  <a href="/demos/geo/addressToAdmin"><i className="browser icon"></i>Address to Administrative Boundaries (uses all available shape files)</a></div>
+                        <div className="item">- <a href="/demos/geo/addressToGADM28Admin"><i className="browser icon"></i>Address to GADM Administrative Boundaries</a></div>
+                        <div className="item">- <a href="/demos/geo/addressToOSMAdmin"><i className="browser icon"></i>Address to OSM Administrative Boundaries</a></div>
+                        <div className="item">- <a href="/demos/geo/addressToFlickrAdmin"><i className="browser icon"></i>Address to Flickr Administrative Boundaries</a></div>
+                        <div className="item">- <a href="/boundariesMap"><i className="browser icon"></i>Example of Administrative Boundaries extracted for a specific country</a></div>
+                        <div className="item">- <a href="/demos/geo/NL_Universities/1000/1000"><i className="browser icon"></i>Educational Institutes in the Netherlands</a></div>
+                        <div className="item">- <a href="https://docs.google.com/document/d/1JoJM7VF_ZaaAPbSjtgpydzRDYLvr-tROzhITGj0cH3w/edit?usp=sharing"><i className="file excel outline icon"></i>Google spreadsheet's add-ons</a></div>
                     </div>
                 </div>
             </div>
