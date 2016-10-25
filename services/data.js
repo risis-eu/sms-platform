e9e27897b9a904acca68436ec636026cd480d87a
ali1k <hyperir@gmail.com>
2016-10-25 15:51:09 +0200

    allowed normal queries

--- services/data.js
+++ services/data.js
@@ -10,7 +10,7 @@ const outputFormat = 'application/sparql-results+json';
 const headers = {'Accept': 'application/sparql-results+json'};
 let user;
 /*-----------------------------------*/
-let endpointParameters, cGraphName, graphName, query, queryObject, utilObject, configurator, propertyURI;
+let endpointParameters, cGraphName, graphName, query, queryObject, utilObject, configurator, datasetURI, entityURI, entityTypeURI, propertyURI, queryParams, smsKey, offsetF, limitF;
 queryObject = new DataQuery();
 utilObject = new DataUtil();
 configurator = new Configurator();
@@ -19,6 +19,7 @@ export default {
     name: 'data',
     // At least one of the CRUD methods is Required
     read: (req, resource, params, config, callback) => {
+        queryParams = req.query;
         if(resource === 'data.datasets'){
             const defaultDatasetListGraph = 'http://rdf.risis.eu/datasets/';
             graphName = (params.id ? decodeURIComponent(params.id) : defaultDatasetListGraph);
@@ -36,14 +37,18 @@ export default {
                 callback(null, {datasets: []});
             });
         } else if (resource === 'data.dataset.entityTypes') {
-            if(!params.smsKey || !isValidAPIToken(params.smsKey)){
+            //set params
+            smsKey = params.smsKey ? params.smsKey : (queryParams.smsKey ? queryParams.smsKey : '');
+            datasetURI = params.datasetURI ? decodeURIComponent(params.datasetURI) : (queryParams.datasetURI ? decodeURIComponent(queryParams.datasetURI) : '');
+            //----------
+            if(!smsKey || !isValidAPIToken(smsKey)){
                 callback(null, {entityTypes: [], error: {'type':'access', 'msg': 'Invalid SMS API Key!'}}); return 0;
             }
             //the URI of graph where data is stored
-            if(!params.datasetURI){
+            if(!datasetURI){
                 callback(null, {entityTypes: [], error: {'type':'params', 'msg': 'datasetURI is not given!'}}); return 0;
             }
-            graphName = decodeURIComponent(params.datasetURI);
+            graphName = datasetURI;
             endpointParameters = getEndpointParameters(graphName);
             //SPARQL QUERY
             query = queryObject.getDatasetEntityTypes(graphName);
@@ -51,39 +56,39 @@ export default {
             //send request
             rp.get({uri: getHTTPQuery('read', query, endpointParameters, outputFormat)}).then(function(res){
                 callback(null, {
-                    datasetURI: graphName,
+                    datasetURI: datasetURI,
                     entityTypes: utilObject.parseDatasetEntityTypes(res),
                 });
             }).catch(function (err) {
                 console.log(err);
-                callback(null, {datasetURI: graphName,entityTypes: []});
+                callback(null, {datasetURI: datasetURI,entityTypes: []});
             });
         } else if (resource === 'data.dataset.entities') {
-            if(!params.smsKey || !isValidAPIToken(params.smsKey)){
+            //set params
+            smsKey = params.smsKey ? params.smsKey : (queryParams.smsKey ? queryParams.smsKey : '');
+            datasetURI = params.datasetURI ? decodeURIComponent(params.datasetURI) : (queryParams.datasetURI ? decodeURIComponent(queryParams.datasetURI) : '');
+            entityTypeURI = params.entityTypeURI ? decodeURIComponent(params.entityTypeURI) : (queryParams.entityTypeURI ? decodeURIComponent(queryParams.entityTypeURI) : '');
+            offsetF = params.offset ? params.offset : (queryParams.offset ? queryParams.offset : 0);
+            limitF = params.limit ? params.limit : (queryParams.limit ? queryParams.limit : 20);
+            //----------
+
+            if(!smsKey || !isValidAPIToken(smsKey)){
                 callback(null, {entities: [], error: {'type':'access', 'msg': 'Invalid SMS API Key!'}}); return 0;
             }
             //the URI of graph where data is stored
-            if(!params.datasetURI || !params.entityTypeURI ){
+            if(!datasetURI || entityTypeURI ){
                 callback(null, {entities: [], error: {'type':'params', 'msg': 'datasetURI or entityTypeURI are not given!'}}); return 0;
             }
-            let offsetF = 0;
-            let limitF = 20 ;
-            if(params.offset){
-                offsetF = params.offset;
-            }
-            if(params.limit){
-                limitF = params.limit;
-            }
-            graphName = decodeURIComponent(params.datasetURI);
+            graphName = datasetURI;
             endpointParameters = getEndpointParameters(graphName);
             //SPARQL QUERY
-            query = queryObject.getDatasetEntities(graphName, decodeURIComponent(params.entityTypeURI), offsetF, limitF);
+            query = queryObject.getDatasetEntities(graphName, decodeURIComponent(entityTypeURI), offsetF, limitF);
             //console.log(query);
             //send request
             rp.get({uri: getHTTPQuery('read', query, endpointParameters, outputFormat)}).then(function(res){
                 callback(null, {
-                    datasetURI: graphName,
-                    entityTypeURI: decodeURIComponent(params.entityTypeURI),
+                    datasetURI: datasetURI,
+                    entityTypeURI: entityTypeURI,
                     offset: offsetF,
                     limit: limitF,
                     entities: utilObject.parseDatasetEntities(res),
@@ -93,23 +98,29 @@ export default {
                 callback(null, {entities: []});
             });
         } else if (resource === 'data.dataset.entity'){
-            if(!params.smsKey || !isValidAPIToken(params.smsKey)){
+            //set params
+            smsKey = params.smsKey ? params.smsKey : (queryParams.smsKey ? queryParams.smsKey : '');
+            datasetURI = params.datasetURI ? decodeURIComponent(params.datasetURI) : (queryParams.datasetURI ? decodeURIComponent(queryParams.datasetURI) : '');
+            entityURI = params.entityURI ? decodeURIComponent(params.entityURI) : (queryParams.entityURI ? decodeURIComponent(queryParams.entityURI) : '');
+            //----------
+
+            if(!smsKey || !isValidAPIToken(smsKey)){
                 callback(null, {entities: [], error: {'type':'access', 'msg': 'Invalid SMS API Key!'}}); return 0;
             }
             //the URI of graph where data is stored
-            if(!params.datasetURI || !params.entityURI ){
+            if(!datasetURI || !entityURI ){
                 callback(null, {entities: [], error: {'type':'params', 'msg': 'datasetURI or entityURI are not given!'}}); return 0;
             }
-            graphName = decodeURIComponent(params.datasetURI);
+            graphName = decodeURIComponent(datasetURI);
             endpointParameters = getEndpointParameters(graphName);
             //SPARQL QUERY
-            query = queryObject.getDatasetEntity(graphName, decodeURIComponent(params.entityURI));
+            query = queryObject.getDatasetEntity(graphName, entityURI);
             //console.log(query);
             //send request
             rp.get({uri: getHTTPQuery('read', query, endpointParameters, outputFormat)}).then(function(res){
                 callback(null, {
-                    datasetURI: graphName,
-                    entityURI: decodeURIComponent(params.entityURI),
+                    datasetURI: datasetURI,
+                    entityURI: entityURI,
                     properties: utilObject.parseDatasetEntity(res),
                 });
             }).catch(function (err) {
