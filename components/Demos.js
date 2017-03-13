6281c99e8dfc2ee3d5b5a0ebb339017aa42c8b50
ali1k <hyperir@gmail.com>
2017-03-13 10:55:27 +0100

    finished demo page

--- components/Demos.js
+++ components/Demos.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {NavLink} from 'fluxible-router';
 
 class Demos extends React.Component {
     componentDidMount() {
@@ -12,41 +13,73 @@ class Demos extends React.Component {
 
         return (
             <div className="ui page grid" ref="demos">
-                <div className="ui column segment">
-                    <div className="ui stackable four cards">
-                        <div className="green card slideInUp animated">
-                          <a className="img-conceptualModel image" href="/demos/geo/addressToAdmin">
-                              <img src="/assets/img/docs/addr2boundary.jpg"/>
-                          </a>
-                          <div className="content center aligned">
-                            <a className="ui" href="/demos/geo/addressToAdmin">Address to Administrative Boundaries (using all open shape files)</a>
-                          </div>
-                        </div>
-                        <div className="green card slideInUp animated">
-                          <a className="img-conceptualModel image" href="/boundariesMap">
-                              <img src="/assets/img/docs/image17.jpg"/>
-                          </a>
-                          <div className="content center aligned">
-                            <a className="ui" href="/boundariesMap">Example of Administrative Boundaries extracted for a specific country</a>
-                          </div>
-                        </div>
-                        <div className="green card slideInUp animated">
-                          <a className="img-conceptualModel image" href="https://docs.google.com/document/d/1JoJM7VF_ZaaAPbSjtgpydzRDYLvr-tROzhITGj0cH3w/edit?usp=sharing">
-                              <img src="/assets/img/docs/image37.png"/>
-                          </a>
-                          <div className="content center aligned">
-                            <a className="ui" href="https://docs.google.com/document/d/1JoJM7VF_ZaaAPbSjtgpydzRDYLvr-tROzhITGj0cH3w/edit?usp=sharing">Google spreadsheet's add-ons for geocoding addresses in a spreadsheet</a>
-                          </div>
-                        </div>
-                        <div className="green card slideInUp animated">
-                          <a className="img-conceptualModel image" href="http://datasets.risis.eu">
-                              <img src="/assets/img/docs/dsp.png"/>
-                          </a>
-                          <div className="content center aligned">
-                            <a className="ui" href="http://datasets.risis.eu">RISIS Datasets Portal derived from the RISIS datasets metadata</a>
-                          </div>
+                <div className="ui column">
+                    <div className="ui massive breadcrumb">
+                      <NavLink routeName="home" className="section" href="/">SMS Platform</NavLink>
+                      <i className="right chevron icon divider"></i>
+                      <div className="active section">Demos</div>
+                    </div>
+                    <div className="ui segment">
+                        <div className="ui stackable four cards">
+                            <div className="blue card slideInUp animated">
+                              <a className="img-conceptualModel image" href="/demos/geo/addressToAdmin">
+                                  <img src="/assets/img/docs/addr2boundary.jpg"/>
+                              </a>
+                              <div className="content center aligned">
+                                <a className="ui" href="/demos/geo/addressToAdmin">Address to Administrative Boundaries (using all open shape files)</a>
+                              </div>
+                            </div>
+                            <div className="blue card slideInUp animated">
+                              <a className="img-conceptualModel image" href="/boundariesMap">
+                                  <img src="/assets/img/docs/image17.jpg"/>
+                              </a>
+                              <div className="content center aligned">
+                                <a className="ui" href="/boundariesMap">Example of Administrative Boundaries extracted for a specific country</a>
+                              </div>
+                            </div>
+                            <div className="blue card slideInUp animated">
+                              <a className="img-conceptualModel image" href="https://docs.google.com/document/d/1JoJM7VF_ZaaAPbSjtgpydzRDYLvr-tROzhITGj0cH3w/edit?usp=sharing">
+                                  <img src="/assets/img/docs/image37.png"/>
+                              </a>
+                              <div className="content center aligned">
+                                <a className="ui" href="https://docs.google.com/document/d/1JoJM7VF_ZaaAPbSjtgpydzRDYLvr-tROzhITGj0cH3w/edit?usp=sharing">Google spreadsheet's add-ons for geocoding addresses in a spreadsheet</a>
+                              </div>
+                            </div>
+                            <div className="blue card slideInUp animated">
+                              <a className="img-conceptualModel image" href="http://datasets.risis.eu">
+                                  <img src="/assets/img/docs/dsp.png"/>
+                              </a>
+                              <div className="content center aligned">
+                                <a className="ui" href="http://datasets.risis.eu">RISIS Datasets Portal derived from the RISIS datasets metadata</a>
+                              </div>
+                            </div>
+                            <div className="blue card slideInUp animated">
+                              <a className="img-conceptualModel image" href="/metadataList">
+                                  <img src="/assets/img/docs/metadataEditor.png"/>
+                              </a>
+                              <div className="content center aligned">
+                                <a className="ui" href="/metadataList">SMS Metadata Editor</a>
+                              </div>
+                            </div>
+                            <div className="blue card slideInUp animated">
+                              <a className="img-conceptualModel image" href="/datasets">
+                                  <img src="/assets/img/docs/image20.jpg"/>
+                              </a>
+                              <div className="content center aligned">
+                                <a className="ui" href="/datasets">Dataset Browser, Editor and Annotator</a>
+                              </div>
+                            </div>
+                            <div className="blue card slideInUp animated">
+                              <a className="img-conceptualModel image" href="/datasets">
+                                  <iframe width="190" height="180" src="https://www.youtube.com/embed/videoseries?list=PLSBPxopOi20XPOn1sGBthbNtXIUOqM_4b" frameBorder="0" allowFullScreen></iframe>
+                              </a>
+                              <div className="content center aligned">
+                                <a className="ui" href="https://www.youtube.com/playlist?list=PLSBPxopOi20XPOn1sGBthbNtXIUOqM_4b">Video Tutorials</a>
+                              </div>
+                            </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         );
