31b439e61a7f7e360c9ebbd7c51d583b0651819f
ali1k <hyperir@gmail.com>
2017-03-13 08:42:23 +0100

    working on the dmeo page

--- components/Demos.js
+++ components/Demos.js
@@ -13,14 +13,39 @@ class Demos extends React.Component {
         return (
             <div className="ui page grid" ref="demos">
                 <div className="ui column segment">
-                    List of Demo Applications that use SMS Linked Data API:<br/>
-                <div className="ui divided animated list">
-                        <div className="item">-  <a href="/demos/geo/addressToAdmin"><i className="browser icon"></i>Address to Administrative Boundaries (uses all available shape files)</a></div>
-                        <div className="item">- <a href="/demos/geo/addressToGADM28Admin"><i className="browser icon"></i>Address to GADM Administrative Boundaries</a></div>
-                        <div className="item">- <a href="/demos/geo/addressToOSMAdmin"><i className="browser icon"></i>Address to OSM Administrative Boundaries</a></div>
-                        <div className="item">- <a href="/demos/geo/addressToFlickrAdmin"><i className="browser icon"></i>Address to Flickr Administrative Boundaries</a></div>
-                        <div className="item">- <a href="/boundariesMap"><i className="browser icon"></i>Example of Administrative Boundaries extracted for a specific country</a></div>
-                        <div className="item">- <a href="https://docs.google.com/document/d/1JoJM7VF_ZaaAPbSjtgpydzRDYLvr-tROzhITGj0cH3w/edit?usp=sharing"><i className="file excel outline icon"></i>Google spreadsheet's add-ons</a></div>
+                    <div className="ui stackable four cards">
+                        <div className="green card slideInUp animated">
+                          <a className="img-conceptualModel image" href="/demos/geo/addressToAdmin">
+                              <img src="/assets/img/docs/addr2boundary.jpg"/>
+                          </a>
+                          <div className="content center aligned">
+                            <a className="ui" href="/demos/geo/addressToAdmin">Address to Administrative Boundaries (using all open shape files)</a>
+                          </div>
+                        </div>
+                        <div className="green card slideInUp animated">
+                          <a className="img-conceptualModel image" href="/boundariesMap">
+                              <img src="/assets/img/docs/image17.jpg"/>
+                          </a>
+                          <div className="content center aligned">
+                            <a className="ui" href="/boundariesMap">Example of Administrative Boundaries extracted for a specific country</a>
+                          </div>
+                        </div>
+                        <div className="green card slideInUp animated">
+                          <a className="img-conceptualModel image" href="https://docs.google.com/document/d/1JoJM7VF_ZaaAPbSjtgpydzRDYLvr-tROzhITGj0cH3w/edit?usp=sharing">
+                              <img src="/assets/img/docs/image37.png"/>
+                          </a>
+                          <div className="content center aligned">
+                            <a className="ui" href="https://docs.google.com/document/d/1JoJM7VF_ZaaAPbSjtgpydzRDYLvr-tROzhITGj0cH3w/edit?usp=sharing">Google spreadsheet's add-ons for geocoding addresses in a spreadsheet</a>
+                          </div>
+                        </div>
+                        <div className="green card slideInUp animated">
+                          <a className="img-conceptualModel image" href="http://datasets.risis.eu">
+                              <img src="/assets/img/docs/dsp.png"/>
+                          </a>
+                          <div className="content center aligned">
+                            <a className="ui" href="http://datasets.risis.eu">RISIS Datasets Portal derived from the RISIS datasets metadata</a>
+                          </div>
+                        </div>
                     </div>
                 </div>
             </div>
