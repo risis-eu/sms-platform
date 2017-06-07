6b569a36d6e97d86cb998ae8bc5a54b92ccea007
ali1k <hyperir@gmail.com>
2017-06-07 15:31:19 +0200

    m

--- services/sparql/FacetQuery.js
+++ services/sparql/FacetQuery.js
@@ -101,7 +101,7 @@ class FacetQuery{
                     //we assume it starts from the original graph
                     //use the default graph name
                         let st_extra = self.makeExtraTypeFilters(endpointParameters, {type: type});
-                        console.log(st_extra);
+                        //console.log(st_extra);
                         qs = `
                             ${st_extra}
                             ?s ${self.filterPropertyPath(part)} ?v${(counter === tmp0.length ? tindex : 'g'+tindex+counter)} .
